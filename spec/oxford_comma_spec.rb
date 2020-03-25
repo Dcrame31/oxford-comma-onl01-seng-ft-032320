93559f5efca8b1bae36026d4d871cd01025a6523
Diane Crame <dcrame@icloud.com>
2020-03-25 00:10:37 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,15 +1,14 @@
-describe "#oxford_comma" do
-  it 'returns a string without any additional fomatting when given a 1-element array' do
-    expect(oxford_comma(["kiwi"])).to eq("kiwi")
-  end
-  it "adds 'and' between elements when given a 2-element array" do
-    expect(oxford_comma(["kiwi", "durian"])).to eq("kiwi and durian")
-  end
-  it "adds commas plus a final 'and' when given a 3-element array" do
-    expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
-  end
-  it "correctly formats arrays of lengths greater than three" do
-    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
-    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
+def oxford_comma(array)
+  array1 = []
+  last_string = array.length - 1
+  counter = 0
+  if array.length == 1
+    puts "#{array[0]}"
+  elsif array.length > 1
+    while counter < last_string
+      array1 << array[counter]
+      counter += 1
+    end
+    puts "#{array1.join(", ")}, and #{array[last_string]}"
   end
 end

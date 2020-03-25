2b495c73f10abd20159426b7bc8fa618360164da
Diane Crame <dcrame@icloud.com>
2020-03-25 00:13:47 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
   array1 = []
-  last_string = array.length - 1
   counter = 0
   if array.length == 1
     puts "#{array[0]}"
   elsif array.length > 1
-    while counter < last_string
+    while counter < array.length-1
       array1 << array[counter]
       counter += 1
     end
-    puts "#{array1.join(", ")}, and #{array[last_string]}"
+    puts "#{array1.join(", ")}, and #{array[array.length-1]}"
   end
 end

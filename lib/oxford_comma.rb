cb368b6bab49058acfb6015ca65f1b0705116be2
Diane Crame <dcrame@icloud.com>
2020-03-25 00:13:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  array1 = []
+  last_string = array.length - 1
+  counter = 0
+  
+  while counter < last_string
+    array1 << array[counter]
+    counter += 1
+  end
+ puts "#{array1.join(", ")}, and #{array[last_string]}"
 end
